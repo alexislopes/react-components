81a40155ed20305954e9086ef8aa20662bb2805b
alexislopes <alexisdrops@gmail.com>
2019-05-24 13:45:20 -0300

    created ApprovalCard component

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import faker from 'faker';
 
 
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard />
             <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post!" avatar={faker.image.avatar()}/>
         </div>
     );

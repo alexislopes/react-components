b05c9354715458b28a0f3df141d5688ed67be6c2
alexislopes <alexisdrops@gmail.com>
2019-05-24 13:51:23 -0300

    passing comment as a ApprovalCard children

--- src/index.js
+++ src/index.js
@@ -10,8 +10,9 @@ import ApprovalCard from './ApprovalCard'
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post!" avatar={faker.image.avatar()}/>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post!" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
         </div>
     );
 }
